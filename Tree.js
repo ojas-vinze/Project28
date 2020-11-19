5483406971d6481d17a41e06bad313f0e99e17e5
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-11-19 18:22:31 +0530

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -8,7 +8,7 @@ class Tree {
       this.width = width;
       this.height = height;
       this.body = Bodies.rectangle(this.x,this.y,this.width,this.height,options);
-      this.image = loadImage("Images/tree.png");
+      this.image = loadImage("tree.png");
       World.add(world, this.body);
     }
     display(){
